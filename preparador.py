8d7f9b1b94b8ca27f8dabb596eb6bd9a0ef14477
MdM2503 <marhaylin@gmail.com>
2017-02-23 21:57:01 +0100

    Add files via upload

--- preparador.py
+++ preparador.py
@@ -1,4 +1,4 @@
-def preparador ( ncaches, tamaniocache, request , n):
+def preparadorgeneral ( ncaches, tamaniocache, request , n):
     # datacenter --> array de latencias por endpoint
     # request ---> array de diccionarios --> indicevideo: (tamanio, indiceendpoint, numrequest)
     # n --> numero de videos que necesito
@@ -11,7 +11,7 @@ def preparador ( ncaches, tamaniocache, request , n):
             #arrayvideos.append(i)
             #arraytamanios.append(request[i][0])
             array.append([sum(request[i][2]), i, request[i][0])])
-
+    #Array : (sumarequest, indicevideo, tamanio)
     #Ahora tengo los que en teoria caben en la caches
     tamaniototal= ncaches * tamaniocache
     array = sorted(array)
@@ -22,4 +22,34 @@ def preparador ( ncaches, tamaniocache, request , n):
             if array [1][2] > array[0][0]:
                 indice = 1
         del array[indice]
-    return [i[1]for i in array]
+    return array
+
+
+def preparadorendpoint ( array, endpoint, request, tamaniocache):
+    # datacenter --> array de latencias por endpoint
+    #endpoint --> indiceendpoint: (indicecahches[0,1..], latencias[100, 200..])
+    # request ---> array de diccionarios --> indicevideo: (tamanio, indiceendpoint, numrequest)
+    # n --> numero de videos que necesito
+    # Comprobar si la latencia del datacenter es mayor que alguna cache--> lo hace el a estrella?
+    # Retorna lista de indices de videos a distribuir
+    # Si hay algun video de mayor peso que las caches, se va a quedar en el datacenter
+    resultado = dict()
+    for i in array:
+        indicevideo = i[1]
+        #Para cada video veo los endpoints que lo piden:
+        indicesendpoints = request[indicevideo][1]
+        for j in indicesendpoints:
+            if j in resultado.keys():
+                temp = resultado[j]
+                temp.append([sum(request[i][2]), i, request[i][0])])
+                resultado[j] = temp
+            else:
+                resultado[j] = [[sum(request[i][2]), i, request[i][0])]]
+    #Aqyui tengo un diccionarios de endpoints: [(request, indice, tamanio)]
+    for i in resultado.keys():
+        datos = resultado[i]
+        datos = sorted(datos)
+        while sum([x[2] for x in datos]) > (len(endpoints[i][0])*tamaniocache):
+            del datos[0]
+        resultados[i]= datos
+    return resultados
